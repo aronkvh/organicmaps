aronkvh/organicmaps

commit 86bbe486431994403852b736801ccd8f2415ee78

[android] Add Android Auto support
Signed-off-by: Andrew Shkrob <andrew.shkrob.social@yandex.by>
Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

android/app/src/main/java/app/organicmaps/MwmActivity.java
@@ -6,6 +6,7 @@ import android.app.Dialog;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Build;
@@ -36,6 +37,7 @@ import androidx.fragment.app.FragmentFactory;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
+
 import app.organicmaps.Framework.PlacePageActivationListener;
 import app.organicmaps.api.Const;
 import app.organicmaps.base.BaseMwmFragmentActivity;
@@ -47,6 +49,9 @@ import app.organicmaps.bookmarks.data.BookmarkInfo;
 import app.organicmaps.bookmarks.data.BookmarkManager;
 import app.organicmaps.bookmarks.data.MapObject;
 import app.organicmaps.bookmarks.data.Track;
+import app.organicmaps.display.DisplayChangedListener;
+import app.organicmaps.display.DisplayManager;
+import app.organicmaps.display.DisplayType;
 import app.organicmaps.downloader.DownloaderActivity;
 import app.organicmaps.downloader.DownloaderFragment;
 import app.organicmaps.downloader.MapManager;
@@ -123,7 +128,7 @@ public class MwmActivity extends BaseMwmFragmentActivity
                CustomNavigateUpListener,
                RoutingController.Container,
                LocationListener,
-    SensorListener,
+               SensorListener,
                LocationState.ModeChangeListener,
                RoutingPlanInplaceController.RoutingPlanListener,
                RoutingBottomMenuListener,
@@ -132,13 +137,15 @@ public class MwmActivity extends BaseMwmFragmentActivity
                NoConnectionListener,
                MenuBottomSheetFragment.MenuBottomSheetInterfaceWithHeader,
                PlacePageController.PlacePageRouteSettingsListener,
-               MapButtonsController.MapButtonClickListener
+               MapButtonsController.MapButtonClickListener,
+               DisplayChangedListener
 {
   private static final String TAG = MwmActivity.class.getSimpleName();
 
   public static final String EXTRA_TASK = "map_task";
   public static final String EXTRA_LAUNCH_BY_DEEP_LINK = "launch_by_deep_link";
   public static final String EXTRA_BACK_URL = "backurl";
+  public static final String EXTRA_UPDATE_THEME = "update_theme";
   private static final String EXTRA_CONSUMED = "mwm.extra.intent.processed";
 
   private static final String[] DOCKED_FRAGMENTS = { SearchFragment.class.getName(),
@@ -216,6 +223,13 @@ public class MwmActivity extends BaseMwmFragmentActivity
   @SuppressWarnings("NotNullFieldNotInitialized")
   @NonNull
   private ActivityResultLauncher<IntentSenderRequest> mLocationResolutionRequest;
+
+  @SuppressWarnings("NotNullFieldNotInitialized")
+  @NonNull
+  private DisplayManager mDisplayManager;
+
+  private boolean mRemoveDisplayListener = true;
+  private int mLastUiMode = Configuration.UI_MODE_TYPE_UNDEFINED;
 
   public interface LeftAnimationTrackListener
   {
@@ -379,6 +393,30 @@ public class MwmActivity extends BaseMwmFragmentActivity
     return super.getThemeResourceId(theme);
   }
 
+  @Override
+  public void onDisplayChangedToCar(@NonNull Runnable onTaskFinishedCallback)
+  {
+    mRemoveDisplayListener = false;
+    startActivity(new Intent(this, MapPlaceholderActivity.class));
+    Objects.requireNonNull(mMapFragment).notifyOnSurfaceDestroyed(onTaskFinishedCallback);
+    finish();
+  }
+
+  @Override
+  public void onConfigurationChanged(@NonNull Configuration newConfig)
+  {
+    super.onConfigurationChanged(newConfig);
+
+    final int newUiMode = newConfig.uiMode & Configuration.UI_MODE_TYPE_MASK;
+    final boolean newUiModeIsCarConnected = newUiMode == Configuration.UI_MODE_TYPE_CAR;
+    final boolean newUiModeIsCarDisconnected = mLastUiMode == Configuration.UI_MODE_TYPE_CAR && newUiMode == Configuration.UI_MODE_TYPE_NORMAL;
+    mLastUiMode = newUiMode;
+
+    if (newUiModeIsCarConnected || newUiModeIsCarDisconnected)
+      return;
+    recreate();
+  }
+
   @SuppressLint("InlinedApi")
   @CallSuper
   @Override
@@ -406,6 +444,9 @@ public class MwmActivity extends BaseMwmFragmentActivity
     mSearchController.getToolbar()
                      .getViewTreeObserver();
 
+    mDisplayManager = DisplayManager.from(this);
+    mDisplayManager.addListener(DisplayType.Device, this);
+
     boolean isLaunchByDeepLink = getIntent().getBooleanExtra(EXTRA_LAUNCH_BY_DEEP_LINK, false);
     initViews(isLaunchByDeepLink);
     updateViewsInsets();
@@ -426,10 +467,17 @@ public class MwmActivity extends BaseMwmFragmentActivity
     if (isConsumed || isFirstLaunch)
       return;
 
-    if (savedInstanceState == null && RoutingController.get().hasSavedRoute())
+    if (RoutingController.get().isPlanning())
+      onPlanningStarted();
+    else if (RoutingController.get().isNavigating())
+      onNavigationStarted();
+    else if (savedInstanceState == null && RoutingController.get().hasSavedRoute())
       addTask(new Factory.RestoreRouteTask());
 
     autostartLocation();
+
+    if (getIntent().getBooleanExtra(EXTRA_UPDATE_THEME, false))
+      ThemeSwitcher.INSTANCE.restart(isMapRendererActive());
   }
 
   private void refreshLightStatusBar()
@@ -1063,7 +1111,7 @@ public class MwmActivity extends BaseMwmFragmentActivity
   protected void onStop()
   {
     super.onStop();
-    Framework.nativeRemovePlacePageActivationListener();
+    Framework.nativeRemovePlacePageActivationListener(this);
     BookmarkManager.INSTANCE.removeLoadingListener(this);
     LocationHelper.from(this).removeListener(this);
     LocationState.nativeRemoveListener();
@@ -1084,14 +1132,16 @@ public class MwmActivity extends BaseMwmFragmentActivity
     mLocationResolutionRequest = null;
     mPostNotificationPermissionRequest.unregister();
     mPostNotificationPermissionRequest = null;
+    if (mRemoveDisplayListener && !isChangingConfigurations())
+      mDisplayManager.removeListener(DisplayType.Device);
   }
 
   @Override
   public void onBackPressed()
   {
-    RoutingController routingController = RoutingController.get();
-    if (!closeBottomSheet(MAIN_MENU_ID) && !closeBottomSheet(LAYERS_MENU_ID) && !collapseNavMenu() &&
-        !closePlacePage() &&!closeSearchToolbar(true, true) &&
+    final RoutingController routingController = RoutingController.get();
+    if (!closeBottomSheet(MAIN_MENU_ID) && !closeBottomSheet(LAYERS_MENU_ID) &&
+        !collapseNavMenu() && !closePlacePage() && !closeSearchToolbar(true, true) &&
         !closeSidePanel() && !closePositionChooser() &&
         !routingController.resetToPlanningStateIfNavigating() && !routingController.cancel())
     {
@@ -1509,7 +1559,7 @@ public class MwmActivity extends BaseMwmFragmentActivity
     refreshLightStatusBar();
 
     // Don't start the background navigation service without fine location.
-    if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED)
+    if (!LocationUtils.checkFineLocationPermission(this))
     {
       Logger.w(LOCATION_TAG, "Permission ACCESS_FINE_LOCATION is not granted, skipping NavigationService");
       return;
@@ -1741,7 +1791,7 @@ public class MwmActivity extends BaseMwmFragmentActivity
   {
     Logger.d(LOCATION_TAG);
 
-    if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED)
+    if (LocationUtils.checkFineLocationPermission(this))
     {
       Logger.i(LOCATION_TAG, "Permission ACCESS_FINE_LOCATION is granted");
       LocationHelper.from(this).start();
@@ -1785,12 +1835,12 @@ public class MwmActivity extends BaseMwmFragmentActivity
       LocationState.nativeOnLocationError(LocationState.ERROR_GPS_OFF);
       LocationHelper.from(this).stop();
     }
-    else if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED)
+    else if (LocationUtils.checkFineLocationPermission(this))
     {
       Logger.i(LOCATION_TAG, "Permission ACCESS_FINE_LOCATION is granted");
       LocationHelper.from(this).start();
     }
-    else if (ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED)
+    else if (LocationUtils.checkCoarseLocationPermission(this))
     {
       Logger.i(LOCATION_TAG, "Permission ACCESS_COARSE_LOCATION is granted");
       LocationHelper.from(this).start();
@@ -1827,9 +1877,7 @@ public class MwmActivity extends BaseMwmFragmentActivity
         Logger.w(LOCATION_TAG, "Permission " + permission + " has been refused");
     }
 
-    // Sic: Android Studio requires explicit calls to checkSelfPermission() for @RequiresPermission in start().
-    if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED ||
-        ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED)
+    if (LocationUtils.checkLocationPermission(this))
     {
       LocationHelper.from(this).start();
       return;

commit 1ac0b3844bfbbd64d3cd8988ecb90b466702850c

[android] Make Drape the source of truth for the location mode changes
Drape changes the location mode under the hood in multiple cases, which
creates the discrepancy between Android <-> Drape state. Trust Drape
and do all location mode transitions only via onMyPositionModeChanged().

This patch makes the app not to start in NOT_FOLLOW_NO_POSITION mode.
Previously it happen because Android read LocationState.getMode()
before initialization of Drape.

This patch also fixes a coding error in checkCoarseLocationPermission(),
introduced by 86bbe48 "[android] Add Android Auto support".

Fixes #1128

Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

android/app/src/main/cpp/app/organicmaps/LocationState.cpp
@@ -35,7 +35,9 @@ Java_app_organicmaps_location_LocationState_nativeSwitchToNextMode(JNIEnv * env,
 JNIEXPORT jint JNICALL
 Java_app_organicmaps_location_LocationState_nativeGetMode(JNIEnv * env, jclass clazz)
 {
-  ASSERT(g_framework, ());
+  // GetMyPositionMode() is initialized only after drape creation.
+  // https://github.com/organicmaps/organicmaps/issues/1128#issuecomment-1784435190
+  ASSERT(g_framework && g_framework->IsDrapeEngineCreated(), ());
   return g_framework->GetMyPositionMode();
 }
 

android/app/src/main/java/app/organicmaps/MwmActivity.java
@@ -120,7 +120,6 @@ import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 import static app.organicmaps.location.LocationState.FOLLOW;
 import static app.organicmaps.location.LocationState.FOLLOW_AND_ROTATE;
 import static app.organicmaps.location.LocationState.LOCATION_TAG;
-import static app.organicmaps.location.LocationState.PENDING_POSITION;
 
 public class MwmActivity extends BaseMwmFragmentActivity
     implements PlacePageActivationListener,
@@ -475,8 +474,6 @@ public class MwmActivity extends BaseMwmFragmentActivity
     else if (savedInstanceState == null && RoutingController.get().hasSavedRoute())
       addTask(new Factory.RestoreRouteTask());
 
-    autostartLocation();
-
     if (getIntent().getBooleanExtra(EXTRA_UPDATE_THEME, false))
       ThemeSwitcher.INSTANCE.restart(isMapRendererActive());
   }
@@ -704,8 +701,9 @@ public class MwmActivity extends BaseMwmFragmentActivity
         Map.zoomOut();
         break;
       case myPosition:
+        Logger.i(LOCATION_TAG, "The location button pressed");
+        // Calls onMyPositionModeChanged(mode + 1).
         LocationState.nativeSwitchToNextMode();
-        startLocation();
         break;
       case toggleMapLayer:
         toggleMapLayerBottomSheet();
@@ -853,7 +851,11 @@ public class MwmActivity extends BaseMwmFragmentActivity
   public void startLocationToPoint(final @Nullable MapObject endPoint)
   {
     closeFloatingPanels();
-    startLocation();
+    if (LocationState.getMode() == LocationState.NOT_FOLLOW_NO_POSITION)
+    {
+      // Calls onMyPositionModeChanged(PENDING_POSITION).
+      LocationState.nativeSwitchToNextMode();
+    }
 
     MapObject startPoint = LocationHelper.from(this).getMyPosition();
     RoutingController.get().prepare(startPoint, endPoint);
@@ -1090,7 +1092,6 @@ public class MwmActivity extends BaseMwmFragmentActivity
     IsolinesManager.from(getApplicationContext()).attach(this::onIsolinesStateChanged);
     LocationState.nativeSetListener(this);
     LocationHelper.from(this).addListener(this);
-    onMyPositionModeChanged(LocationState.nativeGetMode());
     mSearchController.attach(this);
   }
 
@@ -1520,7 +1521,6 @@ public class MwmActivity extends BaseMwmFragmentActivity
 
     mRoutingPlanInplaceController.hideDrivingOptionsView();
     NavigationService.stopService(this);
-    LocationHelper.from(this).restartWithNewMode();
     mMapButtonsViewModel.setSearchOption(null);
     mMapButtonsViewModel.setLayoutMode(MapButtonsController.LayoutMode.regular);
     refreshLightStatusBar();
@@ -1690,16 +1690,56 @@ public class MwmActivity extends BaseMwmFragmentActivity
   @Override
   public void onMyPositionModeChanged(int newMode)
   {
-    Logger.d(LOCATION_TAG, "newMode = " + newMode);
+    Logger.d(LOCATION_TAG, "newMode = " + LocationState.nameOf(newMode));
     mMapButtonsViewModel.setMyPositionMode(newMode);
     RoutingController controller = RoutingController.get();
     if (controller.isPlanning())
       showAddStartOrFinishFrame(controller, true);
-    LocationHelper.from(this).restartWithNewMode();
+
     if (newMode == FOLLOW || newMode == FOLLOW_AND_ROTATE)
       Utils.keepScreenOn(Config.isKeepScreenOnEnabled() || RoutingController.get().isNavigating(), getWindow());
     else
       Utils.keepScreenOn(RoutingController.get().isNavigating(), getWindow());
+
+    final LocationHelper locationHelper = LocationHelper.from(this);
+
+    // Check if location was disabled by the user.
+    if (LocationState.getMode() == LocationState.NOT_FOLLOW_NO_POSITION)
+    {
+      Logger.i(LOCATION_TAG, "Location updates are stopped by the user manually.");
+      if (locationHelper.isActive())
+        locationHelper.stop();
+      return;
+    }
+
+    // Check for any location permissions.
+    if (!LocationUtils.checkCoarseLocationPermission(this))
+    {
+      Logger.w(LOCATION_TAG, "Permissions ACCESS_COARSE_LOCATION and ACCESS_FINE_LOCATION are not granted");
+      // Calls onMyPositionModeChanged(NOT_FOLLOW_NO_POSITION).
+      LocationState.nativeOnLocationError(LocationState.ERROR_DENIED);
+
+      Logger.i(LOCATION_TAG, "Requesting ACCESS_FINE_LOCATION + ACCESS_FINE_LOCATION permissions");
+      dismissLocationErrorDialog();
+      mLocationPermissionRequest.launch(new String[]{
+          ACCESS_COARSE_LOCATION,
+          ACCESS_FINE_LOCATION
+      });
+      return;
+    }
+
+    locationHelper.restartWithNewMode();
+
+    if ((newMode == FOLLOW || newMode == FOLLOW_AND_ROTATE) && !LocationUtils.checkFineLocationPermission(this))
+    {
+      // Try to optimistically request FINE permission for FOLLOW and FOLLOW_AND_ROTATE modes.
+      Logger.i(LOCATION_TAG, "Requesting ACCESS_FINE_LOCATION permission for " + LocationState.nameOf(newMode));
+      dismissLocationErrorDialog();
+      mLocationPermissionRequest.launch(new String[]{
+          ACCESS_COARSE_LOCATION,
+          ACCESS_FINE_LOCATION
+      });
+    }
   }
 
   /**
@@ -1768,30 +1808,6 @@ public class MwmActivity extends BaseMwmFragmentActivity
   }
 
   /**
-   * Start location services when the user presses a button or starts routing.
-   */
-  private void startLocation()
-  {
-    Logger.d(LOCATION_TAG);
-
-    if (LocationUtils.checkFineLocationPermission(this))
-    {
-      Logger.i(LOCATION_TAG, "Permission ACCESS_FINE_LOCATION is granted");
-      LocationHelper.from(this).start();
-      return;
-    }
-
-    // Always try to optimistically request FINE permission when the user presses a button or starts routing.
-    // Android will suppress annoying dialogs and skip directly to onLocationPermissionsResult().
-    Logger.i(LOCATION_TAG, "Requesting ACCESS_FINE_LOCATION permission");
-    dismissLocationErrorDialog();
-    mLocationPermissionRequest.launch(new String[]{
-        ACCESS_COARSE_LOCATION,
-        ACCESS_FINE_LOCATION
-    });
-  }
-
-  /**
    * Request POST_NOTIFICATIONS permission.
    */
   public void requestPostNotificationsPermission()
@@ -1805,42 +1821,6 @@ public class MwmActivity extends BaseMwmFragmentActivity
 
     Logger.i(TAG, "Requesting POST_NOTIFICATIONS permission");
     mPostNotificationPermissionRequest.launch(POST_NOTIFICATIONS);
-  }
-
-  /**
-   * Start location services explicitly on the start of activity.
-   */
-  private void autostartLocation()
-  {
-    if (LocationState.nativeGetMode() == LocationState.NOT_FOLLOW_NO_POSITION)
-    {
-      Logger.i(LOCATION_TAG, "Location updates are stopped by the user manually.");
-      LocationState.nativeOnLocationError(LocationState.ERROR_GPS_OFF);
-      LocationHelper.from(this).stop();
-    }
-    else if (LocationUtils.checkFineLocationPermission(this))
-    {
-      Logger.i(LOCATION_TAG, "Permission ACCESS_FINE_LOCATION is granted");
-      LocationHelper.from(this).start();
-    }
-    else if (LocationUtils.checkCoarseLocationPermission(this))
-    {
-      Logger.i(LOCATION_TAG, "Permission ACCESS_COARSE_LOCATION is granted");
-      LocationHelper.from(this).start();
-    }
-    else
-    {
-      Logger.w(LOCATION_TAG, "Permissions ACCESS_COARSE_LOCATION and ACCESS_FINE_LOCATION are not granted");
-      LocationState.nativeOnLocationError(LocationState.ERROR_DENIED);
-      LocationHelper.from(this).stop();
-
-      Logger.i(LOCATION_TAG, "Requesting ACCESS_FINE_LOCATION + ACCESS_FINE_LOCATION permissions");
-      dismissLocationErrorDialog();
-      mLocationPermissionRequest.launch(new String[]{
-          ACCESS_COARSE_LOCATION,
-          ACCESS_FINE_LOCATION
-      });
-    }
   }
 
   /**
@@ -1862,13 +1842,14 @@ public class MwmActivity extends BaseMwmFragmentActivity
 
     if (LocationUtils.checkLocationPermission(this))
     {
-      LocationHelper.from(this).start();
+      if (LocationState.getMode() == LocationState.NOT_FOLLOW_NO_POSITION)
+        LocationState.nativeSwitchToNextMode();
       return;
     }
 
     Logger.w(LOCATION_TAG, "Permissions ACCESS_COARSE_LOCATION and ACCESS_FINE_LOCATION have been refused");
+    // Calls onMyPositionModeChanged(NOT_FOLLOW_NO_POSITION).
     LocationState.nativeOnLocationError(LocationState.ERROR_DENIED);
-    LocationHelper.from(this).stop();
 
     if (mLocationErrorDialog != null && mLocationErrorDialog.isShowing())
     {
@@ -1929,14 +1910,17 @@ public class MwmActivity extends BaseMwmFragmentActivity
     if (resultCode != Activity.RESULT_OK)
     {
       Logger.w(LOCATION_TAG, "Location resolution has been refused");
+      // Calls onMyPositionModeChanged(NOT_FOLLOW_NO_POSITION).
       LocationState.nativeOnLocationError(LocationState.ERROR_GPS_OFF);
-      LocationHelper.from(this).stop();
       return;
     }
 
     Logger.i(LOCATION_TAG, "Location resolution has been granted, restarting location");
-    LocationHelper.from(this).stop();
-    startLocation();
+    if (LocationState.getMode() == LocationState.NOT_FOLLOW_NO_POSITION)
+    {
+      // Calls onMyPositionModeChanged(PENDING_POSITION).
+      LocationState.nativeSwitchToNextMode();
+    }
   }
 
   /**
@@ -1948,8 +1932,8 @@ public class MwmActivity extends BaseMwmFragmentActivity
   {
     Logger.d(LOCATION_TAG, "settings = " + LocationUtils.areLocationServicesTurnedOn(this));
 
+    // Calls onMyPositionModeChanged(NOT_FOLLOW_NO_POSITION).
     LocationState.nativeOnLocationError(LocationState.ERROR_GPS_OFF);
-    LocationHelper.from(this).stop();
 
     if (mLocationErrorDialog != null && mLocationErrorDialog.isShowing())
     {
@@ -1986,7 +1970,7 @@ public class MwmActivity extends BaseMwmFragmentActivity
       return;
     }
 
-    if (LocationState.nativeGetMode() == LocationState.NOT_FOLLOW_NO_POSITION)
+    if (LocationState.getMode() == LocationState.NOT_FOLLOW_NO_POSITION)
     {
       Logger.d(LOCATION_TAG, "Don't show 'location timeout' error dialog in NOT_FOLLOW_NO_POSITION mode");
       return;
@@ -2011,8 +1995,8 @@ public class MwmActivity extends BaseMwmFragmentActivity
         .setNegativeButton(R.string.current_location_unknown_stop_button, (dialog, which) ->
         {
           Logger.w(LOCATION_TAG, "Disabled by user");
+          // Calls onMyPositionModeChanged(NOT_FOLLOW_NO_POSITION).
           LocationState.nativeOnLocationError(LocationState.ERROR_GPS_OFF);
-          LocationHelper.from(this).stop();
         })
         .setPositiveButton(R.string.current_location_unknown_continue_button, (dialog, which) ->
         {

android/app/src/main/java/app/organicmaps/MwmApplication.java
@@ -363,7 +363,7 @@ public class MwmApplication extends Application implements Application.ActivityL
       Logger.i(LOCATION_TAG, "Android Auto is active, keeping location in the background");
     else if (RoutingController.get().isNavigating())
       Logger.i(LOCATION_TAG, "Navigation is in progress, keeping location in the background");
-    else if (!Map.isEngineCreated() || LocationState.nativeGetMode() == LocationState.PENDING_POSITION)
+    else if (!Map.isEngineCreated() || LocationState.getMode() == LocationState.PENDING_POSITION)
       Logger.i(LOCATION_TAG, "PENDING_POSITION mode, keeping location in the background");
     else
     {

android/app/src/main/java/app/organicmaps/car/CarAppSession.java
@@ -137,7 +137,6 @@ public final class CarAppSession extends Session implements DefaultLifecycleObse
     if (mDisplayManager.isCarDisplayUsed())
     {
       LocationState.nativeSetListener(this);
-      onMyPositionModeChanged(LocationState.nativeGetMode());
       Framework.nativePlacePageActivationListener(this);
     }
     SensorHelper.from(getCarContext()).addListener(this);

android/app/src/main/java/app/organicmaps/car/util/UiHelpers.java
@@ -14,6 +14,7 @@ import androidx.car.app.model.Row;
 import androidx.car.app.navigation.model.MapController;
 import androidx.core.graphics.drawable.IconCompat;
 
+import app.organicmaps.Map;
 import app.organicmaps.R;
 import app.organicmaps.bookmarks.data.MapObject;
 import app.organicmaps.bookmarks.data.Metadata;
@@ -157,7 +158,7 @@ public final class UiHelpers
   private static Action createLocationButton(@NonNull CarContext context)
   {
     final Action.Builder builder = new Action.Builder();
-    final int locationMode = LocationState.nativeGetMode();
+    final int locationMode = Map.isEngineCreated() ? LocationState.getMode() : LocationState.NOT_FOLLOW_NO_POSITION;
     CarColor tintColor = Colors.DEFAULT;
 
     @DrawableRes int drawableRes;

android/app/src/main/java/app/organicmaps/location/LocationHelper.java
@@ -18,6 +18,7 @@ import androidx.core.location.GnssStatusCompat;
 import androidx.core.location.LocationManagerCompat;
 
 import app.organicmaps.Framework;
+import app.organicmaps.Map;
 import app.organicmaps.MwmApplication;
 import app.organicmaps.bookmarks.data.FeatureId;
 import app.organicmaps.bookmarks.data.MapObject;
@@ -109,7 +110,7 @@ public class LocationHelper implements BaseLocationProvider.Listener
   @Nullable
   public MapObject getMyPosition()
   {
-    if (!LocationState.isTurnedOn())
+    if (!isActive())
     {
       mMyPosition = null;
       return null;
@@ -280,7 +281,7 @@ public class LocationHelper implements BaseLocationProvider.Listener
     if (RoutingController.get().isNavigating())
       return INTERVAL_NAVIGATION_MS;
 
-    final int mode = LocationState.nativeGetMode();
+    final int mode = Map.isEngineCreated() ? LocationState.getMode() : LocationState.NOT_FOLLOW_NO_POSITION;
     switch (mode)
     {
       case LocationState.PENDING_POSITION:
@@ -298,11 +299,14 @@ public class LocationHelper implements BaseLocationProvider.Listener
   /**
    * Restart the location with a new refresh interval if changed.
    */
-  @SuppressLint("MissingPermission") // Location permissions have already been granted if isActive() is true.
+  @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
   public void restartWithNewMode()
   {
     if (!isActive())
+    {
+      start();
       return;
+    }
 
     final long newInterval = calcLocationUpdatesInterval();
     if (newInterval == mInterval)
@@ -366,7 +370,14 @@ public class LocationHelper implements BaseLocationProvider.Listener
   {
     if (isActive())
       return;
-    else if (LocationState.nativeGetMode() == LocationState.NOT_FOLLOW_NO_POSITION)
+    else if (!Map.isEngineCreated())
+    {
+      // LocationState.nativeGetMode() is initialized only after drape creation.
+      // https://github.com/organicmaps/organicmaps/issues/1128#issuecomment-1784435190
+      Logger.d(TAG, "Engine is not created yet.");
+      return;
+    }
+    else if (LocationState.getMode() == LocationState.NOT_FOLLOW_NO_POSITION)
     {
       Logger.i(TAG, "Location updates are stopped by the user manually.");
       return;

android/app/src/main/java/app/organicmaps/location/LocationState.java
@@ -4,6 +4,8 @@ import androidx.annotation.IntDef;
 import androidx.annotation.Keep;
 import androidx.annotation.NonNull;
 
+import app.organicmaps.Map;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
@@ -48,7 +50,7 @@ public final class LocationState
 
   public static native void nativeSwitchToNextMode();
   @Value
-  public static native int nativeGetMode();
+  private static native int nativeGetMode();
 
   public static native void nativeSetListener(@NonNull ModeChangeListener listener);
   public static native void nativeRemoveListener();
@@ -63,21 +65,15 @@ public final class LocationState
 
   private LocationState() {}
 
-  /**
-   * Checks if location state on the map is active (so its not turned off or pending).
-   */
-  static boolean isTurnedOn()
+  @Value
+  public static int getMode()
   {
-    return hasLocation(nativeGetMode());
+    if (!Map.isEngineCreated())
+      throw new IllegalStateException("Location mode is undefined until engine is created");
+    return nativeGetMode();
   }
 
-  static boolean hasLocation(int mode)
-  {
-    return (mode > NOT_FOLLOW_NO_POSITION);
-  }
-
-  @SuppressWarnings("unused")
-  static String nameOf(@Value int mode)
+  public static String nameOf(@Value int mode)
   {
     switch (mode)
     {

android/app/src/main/java/app/organicmaps/routing/NavigationService.java
@@ -1,5 +1,6 @@
 package app.organicmaps.routing;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.Manifest.permission.POST_NOTIFICATIONS;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
@@ -241,6 +242,7 @@ public class NavigationService extends Service implements LocationListener
   }
 
   @Override
+  @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
   public void onLocationUpdated(@NonNull Location location)
   {
     // Ignore any pending notifications when service is being stopping.

android/app/src/main/java/app/organicmaps/util/LocationUtils.java
@@ -1,5 +1,6 @@
 package app.organicmaps.util;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
@@ -123,7 +124,7 @@ public class LocationUtils
 
   public static boolean checkCoarseLocationPermission(@NonNull Context context)
   {
-    return ContextCompat.checkSelfPermission(context, ACCESS_FINE_LOCATION) == PERMISSION_GRANTED;
+    return ContextCompat.checkSelfPermission(context, ACCESS_COARSE_LOCATION) == PERMISSION_GRANTED;
   }
 
   public static boolean checkLocationPermission(@NonNull Context context)

android/app/src/main/java/app/organicmaps/widget/menu/MyPositionButton.java
@@ -15,6 +15,8 @@ import androidx.annotation.NonNull;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.core.widget.ImageViewCompat;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import app.organicmaps.Map;
 import app.organicmaps.R;
 import app.organicmaps.location.LocationState;
 import app.organicmaps.util.ThemeUtils;
@@ -36,7 +38,8 @@ public class MyPositionButton
     mButton.setOnClickListener(listener);
     mIcons.clear();
     mFollowPaddingShift = (int) (FOLLOW_SHIFT * button.getResources().getDisplayMetrics().density);
-    update(LocationState.nativeGetMode());
+    final int locationMode = Map.isEngineCreated() ? LocationState.getMode() : LocationState.NOT_FOLLOW_NO_POSITION;
+    update(locationMode);
   }
 
   public void update(int mode)